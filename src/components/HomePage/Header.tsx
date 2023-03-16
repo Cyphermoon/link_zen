4771e3b53ca09c3437d7b7c8acc91a98fc574335
Cypher Moon <temperaturecaleb@gmail.com>
2023-03-16 23:28:10 +0100

    setup gsap

--- src/components/HomePage/Header.tsx
+++ src/components/HomePage/Header.tsx
@@ -1,10 +1,11 @@
+import gsap from 'gsap'
 import Link from 'next/link'
-import React, { useState } from 'react'
-import Button from '../Button'
-import Container from '../Container'
-import Logo from '../Logo'
-import { GiHamburgerMenu } from 'react-icons/gi'
+import { MouseEvent, useState } from 'react'
 import { FaTimes } from 'react-icons/fa'
+import { GiHamburgerMenu } from 'react-icons/gi'
+import Button from '../common/Button'
+import Container from '../common/Container'
+import Logo from '../common/Logo'
 
 interface NavItemProps {
   text: string
@@ -22,14 +23,30 @@ const NavItem = ({ text, href, type = "link" }: NavItemProps) => {
 }
 
 const Header = () => {
-  const [navOpened, setNavOpened] = useState<boolean>(false)
+  const [navOpened, setNavOpened] = useState<boolean | undefined>(undefined)
+
+  const animateIn = (id: string) => {
+    gsap.to(id, {
+      translateX: "0",
+      backgroundColor: "red",
+    })
+  }
+
+  const handleHamburgerClicked = () => {
+    setNavOpened(!navOpened)
+    if (navOpened) {
+    }
+    else {
+      animateIn("#nav_body")
+    }
+  }
 
   return (
     <header className='py-4'>
       <nav >
         <Container className='flex items-center justify-between relative'>
           <Logo />
-          <div className={`absolute bg-primary  ${navOpened ? "flex animate-in slide-in-from-left-full duration-500" : "animate-out slide-out-to-right-full fade-out duration-1000 fill-mode-forwards hidden"} lg:animate-none flex-col items-center justify-center lg:flex left-0 lg:flex-row lg:justify-between lg:static space-y-4 lg:grow lg:space-y-0 top-0 w-screen lg:h-auto lg:w-auto h-screen `}>
+          <div id='nav_body' className={`absolute bg-primary ${navOpened === undefined && "hidden"}  ${navOpened ? "flex animate-in slide-in-from-left-full duration-500" : navOpened != undefined && "animate-out duration-1000 fill-mode-forwards slide-out-to-right-full origin-right"} lg:animate-none flex-col items-center justify-center lg:flex left-0 lg:flex-row lg:justify-between lg:static space-y-4 lg:grow lg:space-y-0 top-0 w-screen lg:h-auto lg:w-auto h-screen `}>
             <ul className='flex lg:space-x-8 space-y-4 lg:space-y-0 mx-auto flex-col lg:flex-row'>
               <NavItem href='#' text='Features' />
               <NavItem href='#' text='About Us' />
@@ -37,7 +54,7 @@ const Header = () => {
             </ul>
             <Button href='#' text="Get Started" />
           </div>
-          <button className='lg:hidden font-bold text-xl z-30' onClick={() => setNavOpened(!navOpened)}>
+          <button className='lg:hidden font-bold text-xl z-30' onClick={handleHamburgerClicked}>
             {
               navOpened ?
                 <FaTimes className={`animate-in spin-in-180 fade-in-0 duration-500`} /> :
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,10 @@
-import Head from 'next/head'
-import * as Popover from '@radix-ui/react-popover';
 import Header from '@/components/HomePage/Header';
-import PageHead from '@/components/PageHead';
+import PageHead from '@/components/common/PageHead';
 
 
 export default function Home() {
   return (
-    <div className='bg-primary h-screen w-screen overflow-x-hidden'>
+    <div className='bg-primary h-screen w-screen'>
       <PageHead title='Organice - Home' />
       <Header />
     </div>
