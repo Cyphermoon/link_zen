566dfc3a4dfde903fd460d9c125d4f9ebd683a5a
Seun Fashina <fashinaoluwaseun36@gmail.com>
2023-03-25 05:47:54 -0700

    created types object

---
+++ src/components/HomePage/Features/type.d.ts
@@ -0,0 +1,12 @@
+type boxContent = {
+    title: string,
+    body: string,
+    children?: React.ReactNode,
+    divStyles?: string,
+    hStyles?: string,
+    pStyles?: string,
+
+}
+
+
+export default boxContent
