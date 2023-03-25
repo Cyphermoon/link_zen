aab845aea737cded47adf9819afbe9b99040ef21
Seun Fashina <fashinaoluwaseun36@gmail.com>
2023-03-25 05:49:49 -0700

    imported Featues into the Home component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,13 @@
 import Header from '@/components/HomePage/Header';
 import PageHead from '@/components/common/PageHead';
-
+import Features from '@/components/HomePage/Features/Features';
 
 export default function Home() {
   return (
     <div className='bg-primary h-screen w-screen'>
       <PageHead title='Organice - Home' />
       <Header />
+      <Features/>
     </div>
   )
 }
