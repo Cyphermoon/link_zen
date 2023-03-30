f0f48038def68b13d49825ebf73a859dcfb4a492
Cypher Moon <temperaturecaleb@gmail.com>
2023-03-31 00:37:16 +0100

    created profile link

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["randomuser.me"]
+    domains: ["randomuser.me", "picsum.photos"]
   }
 }
 
--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,10 +1,12 @@
 import Header from '@/components/Profile/Header'
+import ProfileLinkList from '@/components/Profile/ProfileLinkList'
 import React from 'react'
 
 const Profile = () => {
     return (
         <main className=''>
             <Header />
+            <ProfileLinkList />
         </main>
     )
 }
