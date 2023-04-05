babb51cabcfe01860c58ec73350ade19c738b764
Seun Fashina <fashinaoluwaseun36@gmail.com>
2023-04-05 05:49:26 -0700

    imported Accordion and Footer component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,17 @@
 import Header from '@/components/HomePage/Header';
 import PageHead from '@/components/common/PageHead';
 import Features from '@/components/HomePage/Features/Features';
+import { Accordion } from '@/components/HomePage/Accordion/Accordion';
+import Footer from '@/components/HomePage/Footer';
 
 export default function Home() {
   return (
     <div className='bg-primary w-screen'>
       <PageHead title='Organice - Home' />
       <Header />
-      <Features/>
+      <Features />
+      <Accordion />
+      <Footer/>
     </div>
   )
 }
