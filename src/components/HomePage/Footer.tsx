524101f35f3c9cbd502624dd618c4573bd0369fc
Seun Fashina <fashinaoluwaseun36@gmail.com>
2023-04-05 05:34:04 -0700

    created footer component

---
+++ src/components/HomePage/Footer.tsx
@@ -0,0 +1,14 @@
+import Container from "../common/Container"
+const Footer = () => {
+    return (
+      <footer>
+        <Container className="text-center">
+          <h4 className="mt-6 md:mt-12">ORGANICE</h4>
+          <p className="text-subdue text-base py-6">
+            COPYRIGHT &#169; 2023 - CREATED BY KELVIN AND SEUN
+          </p>
+        </Container>
+      </footer>
+    );
+}
+export default Footer
