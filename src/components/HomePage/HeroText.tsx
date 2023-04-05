f3bdf84cfc587fedbb2d41aac1bbf244f3d2a65c
Seun Fashina <fashinaoluwaseun36@gmail.com>
2023-04-05 05:42:27 -0700

    fixed the propagation bug

--- src/components/HomePage/HeroText.tsx
+++ src/components/HomePage/HeroText.tsx
@@ -1,23 +1,39 @@
 import React from 'react'
 import Button from '../common/Button'
 
+
 interface Props {
-    className?: string
-    headerClassName?: string
+  className?: string;
+  headerClassName?: string;
+  onModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-const HeroText = ({ className, headerClassName }: Props) => {
-    return (
-        <div className={`flex flex-col text-center justify-center items-center ${className}`}>
-            <h1
-                className={`text-primary-900 font-medium lg:font-semibold leading-9 lg:leading-10 mb-6
-                ${headerClassName}`}>
-                <span className='underneath_line'>Organise</span> and Share Your
-                <span className='underneath_line mx-2'>Links</span>  Nicely
-            </h1>
-            <Button text="Get Started" href='#' />
-        </div>
-    )
-}
+const HeroText = ({ className, headerClassName, onModalOpen }: Props) => {
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.stopPropagation();
+    onModalOpen(true);
+  };
+  return (
+    <div
+      className={`flex flex-col text-center justify-center items-center ${className}`}
+    >
+      <h1
+        className={`text-primary-900 font-medium lg:font-semibold leading-9 lg:leading-10 mb-6
+                ${headerClassName}`}
+      >
+        <span className="underneath_line">Organise</span> and Share Your
+        <span className="underneath_line mx-2">Links</span> Nicely
+      </h1>
+
+      <button
+        type="button"
+        onClick={(e) => handleClick(e)}
+        className="bg-accent text-white text-md rounded-lg w-1/2 py-3 px-4"
+      >
+        Join the Waitlist
+      </button>
+    </div>
+  );
+};
 
 export default HeroText
