2387ec91942afc3222dd30dbeba1ad1d7fa56fe8
Seun Fashina <fashinaoluwaseun36@gmail.com>
2023-04-05 05:37:49 -0700

    accepted props that set modal state to true

--- src/components/HomePage/AnimationLayout.tsx
+++ src/components/HomePage/AnimationLayout.tsx
@@ -2,22 +2,24 @@ import React from 'react'
 import Container from '../common/Container'
 import AnimationLink from './AnimationLink'
 import HeroText from './HeroText'
+import { sharedWaitlistProps } from './SpiderLayout'
 
-const AnimationLayout = () => {
-    return (
-        <div className='grow block'>
-            <Container className='relative h-full max-w-5xl text-center flex flex-col justify-center items-center px-2 space-y-12 '>
-                <HeroText headerClassName='text-4xl leading-tight' />
-                <AnimationLink
-                    title='Twitter'
-                    folderName='social media'
-                    dateCreated='22/03/23'
-                    url='https://twitter.com/moon_cypher'
-                    imageUrl='/asset/twitter-preview.svg'
-                    className='' />
-            </Container>
-        </div>
-    )
-}
+const AnimationLayout = ({ openModal }: sharedWaitlistProps) => {
+  return (
+    <div className="grow block">
+      <Container className="relative h-full max-w-5xl text-center flex flex-col justify-center items-center px-2 space-y-12 ">
+        <HeroText onModalOpen={openModal} headerClassName="text-4xl leading-tight" />
+        <AnimationLink
+          title="Twitter"
+          folderName="social media"
+          dateCreated="22/03/23"
+          url="https://twitter.com/moon_cypher"
+          imageUrl="/asset/twitter-preview.svg"
+          className=""
+        />
+      </Container>
+    </div>
+  );
+};
 
 export default AnimationLayout
