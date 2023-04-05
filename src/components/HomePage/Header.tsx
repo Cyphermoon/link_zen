d05c2be00297a804da5356318157f9099a0c66b0
Seun Fashina <fashinaoluwaseun36@gmail.com>
2023-04-05 05:41:20 -0700

    created state to handle modal

--- src/components/HomePage/Header.tsx
+++ src/components/HomePage/Header.tsx
@@ -2,19 +2,28 @@ import { useWindowSize } from 'usehooks-ts'
 import AnimationLayout from './AnimationLayout'
 import NavBar from './NavBar'
 import SpiderLayout from './SpiderLayout'
-
+import { useState } from 'react'
+import WaitListModal from "./modal/Waitlist";
 
 const Header = () => {
-  const lgScreen = 1024
-  const { width } = useWindowSize()
+  const lgScreen = 1024;
+  const { width } = useWindowSize();
+  const [modalState, setModalState] = useState<boolean>(false);
+  const display = modalState
+    ? "animate-in slide-in-from-right-full duration-500"
+    : "animate-moveOut";
 
   return (
-    <header className='py-4 flex flex-col h-screen'>
+    <header
+      className="py-4 flex flex-col h-screen overflow-x-hidden"
+      onClick={() => setModalState(false)}
+    >
       <NavBar />
-      {width >= lgScreen ? <SpiderLayout /> : null}
-      {width < lgScreen ? <AnimationLayout /> : null}
-    </header >
-  )
+      {width >= lgScreen ? <SpiderLayout openModal={setModalState} /> : null}
+      {width < lgScreen ? <AnimationLayout openModal={setModalState} /> : null}
+      <WaitListModal display={display} />
+    </header>
+  );
 }
 
 export default Header
