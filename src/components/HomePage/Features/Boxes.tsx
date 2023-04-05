a67c39f4448d5aee990e059418cbf84978ca67e9
Seun Fashina <fashinaoluwaseun36@gmail.com>
2023-04-05 05:39:04 -0700

    changed the header text styles for the features section

--- src/components/HomePage/Features/Boxes.tsx
+++ src/components/HomePage/Features/Boxes.tsx
@@ -4,7 +4,7 @@ const Box = ({ title, body, children, divStyles, pStyles, hStyles }: boxContent)
   return (
     <div className={divStyles ? divStyles : 'flex flex-col bg-primary rounded-xl items-center text-center p-3 md:py-4 md:px-16 shadow-md h-72'}>
       {children}
-      <h4 className={hStyles ? hStyles : "my-auto"}>{title}</h4>
+      <h5 className={hStyles ? hStyles : "my-10 text-2xl font-medium"}>{title}</h5>
       <p className={pStyles ? pStyles : "text-center"}>{body}</p>
     </div>
   );
