7bbfb896fb809b386f1f01c5395596a938d0ea86
Seun Fashina <fashinaoluwaseun36@gmail.com>
2023-04-06 04:53:07 -0700

    restyled the box components in the features section

--- src/components/HomePage/Features/Boxes.tsx
+++ src/components/HomePage/Features/Boxes.tsx
@@ -2,9 +2,9 @@ import boxContent from "./type";
 
 const Box = ({ title, body, children, divStyles, pStyles, hStyles }: boxContent) => {
   return (
-    <div className={divStyles ? divStyles : 'flex flex-col bg-primary rounded-xl items-center text-center p-3 md:py-4 md:px-16 shadow-md h-72'}>
+    <div className={divStyles ? divStyles : 'flex flex-col bg-primary rounded-xl items-center text-center p-3 md:px-16 shadow-md'}>
       {children}
-      <h5 className={hStyles ? hStyles : "my-10 text-2xl font-medium"}>{title}</h5>
+      <h5 className={hStyles ? hStyles : "my-7 md:my-5 text-2xl font-medium"}>{title}</h5>
       <p className={pStyles ? pStyles : "text-center"}>{body}</p>
     </div>
   );
