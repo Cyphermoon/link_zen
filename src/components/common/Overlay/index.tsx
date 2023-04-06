58f341d25b8d77951acc04930dd7b94c0ca027df
Seun Fashina <fashinaoluwaseun36@gmail.com>
2023-04-06 04:46:12 -0700

    created overlay component for the waitlist modal

---
+++ src/components/common/Overlay/index.tsx
@@ -0,0 +1,18 @@
+interface Props {
+  handleClick: () => void;
+  children: React.ReactNode;
+  className?: string;
+}
+
+const Overlay = ({ handleClick, children, className }: Props) => {
+  return (
+    <div
+      onClick={handleClick}
+      className={`fixed w-screen h-screen fade-in z-[100] top-0 flex items-center justify-center ${className}`}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Overlay;
