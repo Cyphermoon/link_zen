bf8a9f9149f68de50ea07b90a35f8a069f125bff
Cypher Moon <temperaturecaleb@gmail.com>
2023-04-13 22:54:44 +0100

    added tilt effect to build profile card

--- src/components/HomePage/Features/Features.tsx
+++ src/components/HomePage/Features/Features.tsx
@@ -6,53 +6,55 @@ import { IoColorPaletteOutline } from "react-icons/io5";
 
 
 import Box from './Boxes';
+import { useTiltEffect } from '@/hooks/index.hook';
 
 
 const Features = () => {
-    return (
-      <main>
-        <Container>
-          <h3 className="text-center my-12 md:my-24">Features</h3>
-          <div className="flex flex-col md:grid md:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-12">
-            <Box
-              title="Url Shortner"
-              body="You can shorten url you think is too long"
-            >
-              <FiLink2 className="text-7xl" />
-            </Box>
 
-            <Box
-              title="Link Visibility"
-              body="You can make your links public or private"
-            >
-              <MdOutlineVisibility className="text-7xl" />
-            </Box>
+  const { tiltCard, removeTiltEffect } = useTiltEffect()
 
-            <Box
-              title="Build Profile Page"
-              body="Showcase essential links on your profile and share them with the entire world"
-              divStyles="flex flex-col  md:col-span-2 order-last lg:order-none lg:row-span-2 lg:col-auto profile-gradient rounded-xl items-center justify-center p-3 md:py-4 md:px-12 shadow-md h-72 lg:h-full"
-              hStyles="my-8 text-2xl text-center font-medium"
-              pStyles="text-center text-lg"
-            />
+  return (
+    <main>
+      <Container>
+        <h3 className="text-center my-12 md:my-24">Features</h3>
+        <div className="flex flex-col md:grid md:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-12" style={{ perspective: "800px" }}>
+          <Box
+            title="Url Shortner"
+            body="You can shorten url you think is too long"
+          >
+            <FiLink2 className="text-7xl" />
+          </Box>
 
-            <Box
-              title="Color Tag"
-              body="You can customize the color of your links according to your style"
-            >
-              <IoColorPaletteOutline className="text-7xl" />
-            </Box>
+          <Box
+            title="Link Visibility"
+            body="You can make your links public or private"
+          >
+            <MdOutlineVisibility className="text-7xl" />
+          </Box>
 
-            <Box
-              title="Multi-Link Creation"
-              body="You can create multiple links in a row or provide a json, csv file"
-            >
-              <GrMultiple className="text-7xl font-thin" />
-            </Box>
+          <div onMouseMove={tiltCard} onMouseOut={removeTiltEffect} className="flex flex-col  md:col-span-2 order-last lg:order-none lg:row-span-2 lg:col-auto profile-gradient rounded-xl items-center justify-center p-3 md:py-4 md:px-12 shadow-md h-72 lg:h-full">
+            <h5 className={"my-8 text-2xl text-center font-medium"}>Build Profile Page</h5>
+            <p className={"text-center text-lg"}>Showcase essential links on your profile and share them with the entire world</p>
           </div>
-        </Container>
-      </main>
-    );
+
+
+          <Box
+            title="Color Tag"
+            body="You can customize the color of your links according to your style"
+          >
+            <IoColorPaletteOutline className="text-7xl" />
+          </Box>
+
+          <Box
+            title="Multi-Link Creation"
+            body="You can create multiple links in a row or provide a json, csv file"
+          >
+            <GrMultiple className="text-7xl font-thin" />
+          </Box>
+        </div>
+      </Container>
+    </main>
+  );
 }
 
 export default Features
--- src/components/HomePage/NavBar/index.tsx
+++ src/components/HomePage/NavBar/index.tsx
@@ -39,13 +39,13 @@ const NavBar = () => {
 
     return (
         <nav className='mb-10' >
-            <Container className='flex items-center justify-between relative'>
+            <Container className='flex items-center justify-between'>
                 <Logo />
 
                 <div
                     className={`
-                    absolute flex bg-primary 
-                    left-0 md:-left-20 lg:animate-none flex-col items-center z-20 lg:z-auto justify-center lg:flex lg:flex-row lg:justify-between lg:static space-y-4 lg:grow lg:space-y-0 top-0 w-screen lg:h-auto lg:w-auto h-screen
+                    fixed flex bg-primary 
+                    left-0  lg:animate-none flex-col items-center z-20 lg:z-auto justify-center lg:flex lg:flex-row lg:justify-between lg:static space-y-4 lg:grow lg:space-y-0 top-0 w-screen lg:h-auto lg:w-auto h-screen
                     ${navOpened === undefined && "hidden"}  
                     ${navOpened ? "animate-in slide-in-from-left-full duration-500" : navOpened != undefined && "animate-moveOut"} 
                     `}>
@@ -58,7 +58,7 @@ const NavBar = () => {
                     <Button href='/get-started' text="Get Started" tabIndex={getTabIndex()} />
                 </div>
 
-                <button className='lg:hidden font-bold text-xl z-30' onClick={handleHamburgerClicked}>
+                <button className={`lg:hidden font-bold text-xl z-30 ${navOpened ? "fixed top-5 right-10" : "static"}`} onClick={handleHamburgerClicked}>
                     {
                         navOpened ?
                             <FaTimes className={`animate-in spin-in-180 fade-in-0 duration-500`} /> :
