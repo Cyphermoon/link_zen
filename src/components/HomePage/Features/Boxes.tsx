2ad9299ba12cb18c2f968c5a6c240c0cda131b80
Seun Fashina <fashinaoluwaseun36@gmail.com>
2023-11-06 14:13:31 -0800

    updated features

--- src/components/HomePage/Features/Boxes.tsx
+++ src/components/HomePage/Features/Boxes.tsx
@@ -1,12 +1,55 @@
 import boxContent from "./type";
+import {
+  nunito,
+  poppins,
+  roboto,
+  opensans,
+  dmSans,
+  urbanist,
+  worksans,
+  pfDisplay,
+  spectral,
+  karla,
+  raleway,
+  montserrat,
+} from "font";
 
-const Box = ({ title, body, children, divStyles, pStyles, hStyles }: boxContent) => {
+const Box = ({
+  title,
+  body,
+  children,
+  divStyles,
+  pStyles,
+  hStyles,
+}: boxContent) => {
   return (
-    <div className={divStyles ? divStyles : 'flex flex-col bg-primary rounded-xl items-center justify-start text-center p-3 md:px-8 shadow-md transition-transform hover:-translate-y-3 text-primary-900'}>
+    <div
+      className={
+        divStyles
+          ? divStyles
+          : "flex flex-col md:bg-primary rounded-md md:rounded-xl md:items-center justify-start md:text-center py-5 px-3 md:py-3 md:px-8 md:shadow-md transition-transform hover:-translate-y-3 text-primary-900"
+      }
+    >
       {children}
-      <div className="space-y-3 mt-8">
-        <h5 className={hStyles ? hStyles : "text-2xl font-medium"}>{title}</h5>
-        <p className={pStyles ? pStyles : "text-center"}>{body}</p>
+      <div className="space-y-3 mt-4 md:mt-8">
+        <h5
+          className={
+            hStyles
+              ? hStyles
+              : `ormal-case text-2xl font-semibold ${urbanist.className}`
+          }
+        >
+          {title}
+        </h5>
+        <p
+          className={
+            pStyles
+              ? pStyles
+              : `md:text-center ${montserrat.className} md:font-medium`
+          }
+        >
+          {body}
+        </p>
       </div>
     </div>
   );
--- src/components/HomePage/Features/Features.tsx
+++ src/components/HomePage/Features/Features.tsx
@@ -1,43 +1,72 @@
-import { FiLink2 } from 'react-icons/fi';
+import { FiLink2 } from "react-icons/fi";
 import { GrMultiple } from "react-icons/gr";
-import { IoColorPaletteOutline } from "react-icons/io5";
+import { IoColorPaletteOutline, IoAnalytics } from "react-icons/io5";
 import { MdOutlineVisibility } from "react-icons/md";
-import { BsBell } from 'react-icons/bs'
+import { BsBell } from "react-icons/bs";
 import { VscPreview } from "react-icons/vsc";
-import { TbFolders } from 'react-icons/tb'
-import { AiOutlineShop } from 'react-icons/ai'
-import { MdOutlineMobileScreenShare } from 'react-icons/md'
-import Container from '../../common/Container';
+import { TbFolders } from "react-icons/tb";
+import { AiOutlineShop } from "react-icons/ai";
+import { IoExtensionPuzzleOutline } from "react-icons/io5";
+import { MdOutlineMobileScreenShare } from "react-icons/md";
+import Container from "../../common/Container";
+import { opensans, pfDisplay, lato, quicksand } from "font";
 
-
-import Box from './Boxes';
-
+import Box from "./Boxes";
 
 const Features = () => {
-
   return (
     <main id="features">
       <Container>
-        <h3 className="text-center mb-12">Features</h3>
-        <div className="flex flex-col md:grid md:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-12">
+        <h3
+          className={`text-4xl font-semibold normal-case ${pfDisplay.className}`}
+        >
+          Make your life easy with LinkZen
+        </h3>
+        <p
+          className={`text-lg mt-4 mb-12 md:text-xl font-medium text-gray-500 ${quicksand.className}`}
+        >
+          Life is hard but we can make it easy
+        </p>
+        <div className="flex flex-col md:grid md:grid-cols-2 lg:grid-cols-4 gap-6 md:gap-12">
           <Box
-            title="Url Shortner"
-            body="Easily reduce the length of your links with our url shortner"
+            title="Save to LinkZen Extension"
+            body="Save web content directly to your LinkZen profile while browsing, using LinkZen's chrome extention"
           >
-            <FiLink2 className="text-7xl" />
+            <div className="p-1 w-20 rounded-md flex items-center justify-center bg-profile-gradient md:bg-none">
+              <IoExtensionPuzzleOutline className="text-5xl md:text-7xl text-neutral-500" />
+            </div>
           </Box>
           <Box
+            title="Analytics"
+            body="Gain valuable insights into your content's impact with detailed analytics. Understand your audience and your resource performance"
+          >
+            <div className="bg-profile-gradient p-1 w-20 rounded-md flex items-center justify-center md:bg-none">
+              <IoAnalytics className="text-5xl md:text-7xl text-green-900" />
+            </div>
+          </Box>
+          <Box
+            title="Url shortner"
+            body="Easily reduce the length of your links with our url shortner for easy sharing of your links"
+          >
+            <div className="bg-profile-gradient p-1 w-20 rounded-md flex items-center justify-center md:bg-none">
+              <FiLink2 className="text-5xl md:text-7xl text-blue-950" />
+            </div>
+          </Box>
+          <Box
+            title="Inbuilt Browser"
+            body="View the content of external websites directly within LinkZen, without leaving the page"
+          >
+            <div className="bg-profile-gradient p-1 w-20 rounded-md flex items-center justify-center md:bg-none">
+              <VscPreview className="text-5xl md:text-7xl text-stone-700" />
+            </div>
+          </Box>
+          {/* <Box
             title="Link Visibility"
             body="Choose who can see your links by making them public or private"
           >
             <MdOutlineVisibility className="text-7xl" />
           </Box>
-          <Box
-            title="Inbuilt Browser"
-            body="View the content of your links without leaving the page"
-          >
-            <VscPreview className="text-7xl" />
-          </Box>
+
           <Box
             title="Nested Folders"
             body="Easily create folders in folders to store even more related links"
@@ -56,11 +85,11 @@ const Features = () => {
             body="Get notifications from those you subscribe to, you can also choose to unsubscribe anytime"
           >
             <BsBell className="text-7xl font-thin" />
-          </Box>
+          </Box> */}
         </div>
       </Container>
     </main>
   );
-}
+};
 
-export default Features
+export default Features;
