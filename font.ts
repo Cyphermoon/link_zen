a027fef1eb692b158606854789e0a761ca029ccb
Seun Fashina <fashinaoluwaseun36@gmail.com>
2023-11-06 14:11:45 -0800

    added new fonts

--- font.ts
+++ font.ts
@@ -1,34 +1,109 @@
-import {  Urbanist, DM_Sans, Roboto, Poppins, Nunito } from 'next/font/google';
- 
+import {
+  Urbanist,
+  DM_Sans,
+  Roboto,
+  Poppins,
+  Nunito,
+  Open_Sans,
+  Lato,
+  Quicksand,
+  Work_Sans,
+  Playfair_Display,
+  Spectral,
+  Karla,
+  Raleway,
+  Montserrat,
+} from "next/font/google";
+
 export const poppins = Poppins({
-  subsets: ['latin'],
-  display: 'swap',
+  subsets: ["latin"],
+  display: "swap",
   variable: "--font-inter",
-  weight: ["100", "200", "300", "400", "500"]
+  weight: ["100", "200", "300", "400", "500"],
 });
- 
+
+export const spectral = Spectral({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-inter",
+  weight: ["200", "300", "400", "500"],
+});
+
+export const montserrat = Montserrat({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-inter",
+  weight: ["200", "300", "400", "500"],
+});
+export const raleway = Raleway({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-inter",
+  weight: ["200", "300", "400", "500"],
+});
+
+export const worksans = Work_Sans({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-inter",
+  weight: ["200", "300", "400", "500"],
+});
+export const opensans = Open_Sans({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-inter",
+  weight: ["300", "400", "500"],
+});
+
+export const karla = Karla({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-inter",
+  weight: ["200", "300", "400", "500"],
+});
+
+export const pfDisplay = Playfair_Display({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-inter",
+  weight: ["400", "500"],
+});
+
 export const urbanist = Urbanist({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: "--font-urbanist"
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-urbanist",
 });
 
 export const dmSans = DM_Sans({
-  subsets: ['latin'],
-  display: 'swap',
+  subsets: ["latin"],
+  display: "swap",
   variable: "--font-dmSans",
-  weight: ["400", "500", "700"]
+  weight: ["400", "500", "700"],
 });
 
 export const roboto = Roboto({
-  subsets: ['latin'],
-  display: 'swap',
+  subsets: ["latin"],
+  display: "swap",
   variable: "--font-roboto",
-  weight: ["400", "500", "700"]
+  weight: ["400", "500", "700"],
 });
 
 export const nunito = Nunito({
   subsets: ["latin"],
   display: "swap",
-  variable: "--font-nunito"
-})
+  variable: "--font-nunito",
+});
+
+export const lato = Lato({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-nunito",
+  weight: "100",
+});
+
+export const quicksand = Quicksand({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-nunito",
+});
