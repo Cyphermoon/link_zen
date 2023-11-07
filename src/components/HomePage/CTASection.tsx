be9db645dd66dee9a8d6d14bec582f322ccb9cbe
Seun Fashina <fashinaoluwaseun36@gmail.com>
2023-11-07 14:43:02 -0800

    updated content

--- src/components/HomePage/CTASection.tsx
+++ src/components/HomePage/CTASection.tsx
@@ -3,6 +3,7 @@ import Container from '../common/Container'
 
 import { useWaitListContext } from '@/context/WaitlistProvider'
 import Btn from '../common/Button'
+import { urbanist, raleway } from 'font'
 
 const CTASection = () => {
     const { openWaitList } = useWaitListContext()
@@ -14,8 +15,8 @@ const CTASection = () => {
     return (
         <section className='bg-accent text-white pt-5 lg:pt-10  pb-10 lg:pb-10'>
             <Container className='flex flex-col lg:flex-row justify-between items-center text-center lg:text-left space-y-5 lg:space-y-0' >
-                <h2 className='font-semibold lg:font-bold text-4xl lg:text-5xl lg:leading-tight w-full lg:w-6/12'>
-                    Get organised with your content today
+                <h2 className={`${raleway.className} normal-case font-semibold lg:font-bold text-4xl lg:text-5xl lg:leading-tight w-full lg:w-6/12`}>
+                    Get your content out there today!
                 </h2>
                 <Btn type='button' handleClick={handleWaitlistClicked} variant="inverse">
                     Join the Waitlist
